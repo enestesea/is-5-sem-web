9562d4061482616a1b9e54634b1e670815493601
enestesea <mitnas03@mail.ru>
2022-11-06 01:19:17 +0300

    feat: made starting-window

---
+++ scripts/starting-window.js
@@ -0,0 +1,7 @@
+function eighteen_yes(){
+    document.getElementsByClassName("starting-window")[0].style.display = "none";
+}
+
+function eighteen_no(){
+    window.open("https://www.google.com/search?q=%D0%BD%D0%BE%D0%B6%D0%BA%D0%B8+%D1%81%D0%BE%D0%BD%D0%B8%D0%BA%D0%B0&client=safari&rls=en&sxsrf=ALiCzsabZFHVtwO8-_gwuV3clU8Ohb6ygQ:1667680505649&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiw9IyB8pf7AhVxposKHe4JCrUQ_AUoAXoECAIQAw&biw=1440&bih=820&dpr=2")
+}
