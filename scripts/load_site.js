a4dcdede2ae853a8a58e5e8ffd0ecc920e06c623
enestesea <mitnas03@mail.ru>
2022-11-07 11:33:20 +0300

    fix: fixed typos

--- scripts/load_site.js
+++ scripts/load_site.js
@@ -1,5 +1,5 @@
 window.startTime = (new Date).getTime()
 window.onload = function () {
     let pageloadtime = ((new Date).getTime() - window.startTime)/1000;
-    document.getElementsByClassName("footer__page-load-time")[0].innerText = 'Время заячгрузки сайта: ' + pageloadtime + ' Данилы Зубовых';
+    document.getElementsByClassName("footer__page-load-time")[0].innerText = 'Время загрузки сайта: ' + pageloadtime + ' Данилы Зубовых';
 }
