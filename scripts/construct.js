197496273a97fc97c758527ef613971497385561
enestesea <mitnas03@mail.ru>
2022-11-20 01:50:29 +0300

    feat: added localStorage

--- scripts/construct.js
+++ scripts/construct.js
@@ -1,11 +1,20 @@
 const todoList = document.getElementsByClassName("constructor__todo")[0];
 const todoInput = document.getElementsByClassName("constructor__form")[0];
+let todoArr = [];
 
 
 todoInput.addEventListener("submit", function (e){
     e.preventDefault();
     const formData = new FormData(todoInput);
-    const groupname = formData.get("groupname");
-    const songname = formData.get("songname");
-    todoList.append(groupname + songname);
+    let todo = {
+        groupname: formData.get("groupname"),
+        songname: formData.get("songname")
+    };
+    todoArr.push(todo);
+    Store(todoList);
 });
+
+
+function Store(listTodo){
+    localStorage.setItem("listTodo", listTodo);
+}
