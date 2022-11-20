b25f86066472f56def63b7a2087e930b1fa7da82
enestesea <mitnas03@mail.ru>
2022-11-20 23:02:12 +0300

    fix: fixed getting items from the storage

--- scripts/construct.js
+++ scripts/construct.js
@@ -14,18 +14,26 @@ todoInput.addEventListener("submit", function (e){
     };
     todoArr.push(todo);
     todoInput.reset();
-    store(todoList);
+    store(todoArr);
 });
 
+todoList.addEventListener("click", function(event){
+    if (event.classList.contains("delete__button)")){
+        deleteTodo(event.target.parentElement.getAttribute("id"));
+    }
+})
 
-function store(listTodo){
-    localStorage.setItem("listTodo", listTodo);
+
+function store(todoArr){
+    localStorage.setItem("listTodo", JSON.stringify(todoArr));
+    displayTodoList(todoArr);
 }
 
 function getFromStorage(){
     const ref = localStorage.getItem("listTodo");
     if (ref){
-        displayTodoList(ref);
+        todoArr = JSON.parse(ref);
+        displayTodoList(todoArr);
     }
 }
 
@@ -33,15 +41,29 @@ function clearStorage(){
     localStorage.removeItem("listTodo");
 }
 
-getFromStorage();
-
-function displayTodoList(list){
-
+function displayTodoList(todoArr){
+    todoList.innerHTML = '';
+    todoArr.forEach(function (todo) {
+        const div = document.createElement("div");
+        const done = todo.done ? 'checked' : null;
+        // if (todo.done === true){
+        //     div.classList.add(".done__todo");
+        // }
+        div.setAttribute("id", todo.id);
+        div.innerHTML = `
+            <input type="checkbox" class="checkbox_${todo.id}">
+            ${todo.groupname} ${todo.songname}
+            <button class="delete__button"">Delete</button>
+        `;
+        todoList.append(div);
+    })
 }
 
 function deleteTodo(id){
-    newTodoArr = todoArr.filter(function (todo){
+    let newTodoArr = todoArr.filter(function (todo){
         return todo.id !== id;
     });
     store(newTodoArr);
 }
+
+getFromStorage();
