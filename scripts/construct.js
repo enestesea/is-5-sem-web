f9e60323b255c13d5108faf18eec849ee316d5b3
enestesea <mitnas03@mail.ru>
2022-12-05 00:36:43 +0300

    fix: added toggling checkbox

--- scripts/construct.js
+++ scripts/construct.js
@@ -6,15 +6,22 @@ let todoArr = [];
 todoInput.addEventListener("submit", function (e){
     e.preventDefault();
     const formData = new FormData(todoInput);
-    const todo = {
-        groupname: formData.get("groupname"),
-        songname: formData.get("songname"),
-        done: false,
-        id: String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Date.now()
-    };
-    todoArr.push(todo);
-    todoInput.reset();
-    store(todoArr);
+    let groupname = formData.get("groupname");
+    let songname = formData.get("songname");
+    if (groupname === "" || songname === ""){
+        alert("placeholder all fields");
+    }
+    else {
+        const todo = {
+            groupname: groupname,
+            songname: songname,
+            done: false,
+            id: String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Date.now()
+        };
+        todoArr.push(todo);
+        todoInput.reset();
+        store(todoArr);
+    }
 });
 
 function store(todoArr){
@@ -40,11 +47,11 @@ function displayTodoList(todoArr){
     todoList.innerHTML = '';
     todoArr.forEach(function (todo) {
         const div = document.createElement("div");
-        div.setAttribute("class", "todo-list__item")
-        const done = todo.done ? 'checked' : null;
+        const done = todo.done ? "checked" : null;
+        div.setAttribute("class", "todo-list__item");
         div.setAttribute("id", todo.id);
         div.innerHTML = `
-            <input type="checkbox" class="checkbox ${todo.id}">
+            <input type="checkbox" class="checkbox" ${done}>
             ${todo.groupname} ${todo.songname}
             <button class="delete__button">Delete</button>
         `;
@@ -59,11 +66,25 @@ function deleteTodo(id){
     store(todoArr);
 }
 
+function toggle(id){
+    for (let i = 0; i < todoArr.length; i++){
+        if (todoArr[i].id === id){
+            todoArr[i].done = !todoArr[i].done;
+        }
+    }
+    store(todoArr);
+}
+
 getFromStorage();
 
 todoList.addEventListener('click', (event) => {
     const isButton = event.target.classList.contains("delete__button");
+    const isCheckBox = event.target.classList.contains("checkbox");
     if (isButton){
         deleteTodo(event.target.parentElement.id);
+    }
+
+    if(isCheckBox){
+        toggle(event.target.parentElement.id);
     }
 })
